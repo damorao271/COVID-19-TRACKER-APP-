cf61603fc96852f15d8eac7443d0ecccdb76df64
Daniel Morao <13-10932@usb.ve>
2020-05-23 17:42:51 -0400

    Charts Ready

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api/index";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  });
+
+  const lineChart = <Line data={{ labels: "", datasets: [{}, {}] }} />;
+
   return (
     <div>
       <h1>Chart</h1>
